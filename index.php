750b4de40a844000cd92fffee0a4f154111ee105
Enno Welbers <enno.welbers@googlemail.com>
2014-02-27 09:33:14 +0100

    prepared for warzone

--- index.php
+++ index.php
@@ -52,8 +52,8 @@ if(isset($_POST['url']))
 			{
 		?>
 		<li>
-			<a href="<?php echo $items[$i]->get_permalink();?>"><?php echo $items[$i]->get_title();?></a>
-			<div class="content"><?php echo $items[$i]->get_content(); ?>
+			<a href="<?php echo $items[$i]->get_permalink();?>"><?= $items[$i]->get_title();?></a>
+			<div class="content"><?=$items[$i]->get_content(); ?>
 			</div>
 		</li>
 		<?php
